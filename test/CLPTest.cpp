3c0250d25fa7f5f29f51a8fdbb64a37cd7ff15a9
fkretschmer <fleming.kretschmer@uni-jena.de>
2019-10-27 18:11:56 +0100

    JNI (instead of JNA) solution, better row caching

--- test/CLPTest.cpp
+++ test/CLPTest.cpp
@@ -15,11 +15,11 @@ TEST_CASE("Feasible ILP") {
   model.setColBounds(col_lb, col_ub, 2);
   // add rows
   std::vector<double> row1{4.0, 5.0};
-  model.addRow(&row1[0], 2, -infty, 20.0);
+  model.addFullRow(&row1[0], 2, -infty, 20.0);
   std::vector<double> row2{10.0, 7.0};
-  model.addRow(&row2[0], 2, -infty, 35.0);
+  model.addFullRow(&row2[0], 2, -infty, 35.0);
   std::vector<double> row3{3.0, 4.0};
-  model.addRow(&row3[0], 2, 6.0, infty);
+  model.addFullRow(&row3[0], 2, 6.0, infty);
   // col start
   std::vector<double> colStart{88.0, 200.0};
   model.setColStart(&colStart[0], colStart.size());
@@ -43,7 +43,7 @@ TEST_CASE("Infeasible ILP LP feasible") {
   model.setColBounds(col_lb, col_ub, 1);
   // add rows
   std::vector<double> row1{10.0};
-  model.addRow(&row1[0], 1, 3.0, 5.0);
+  model.addFullRow(&row1[0], 1, 3.0, 5.0);
   // solve
   REQUIRE(model.solve() == CLPModel::RET_INFEASIBLE);
   delete[] objective;
