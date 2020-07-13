4e2a08b2347ab59c5b12719f241e008a3c67b743
fkretschmer <fleming.kretschmer@uni-jena.de>
2020-07-13 12:59:57 +0200

    Adapted implementation to use wrapper pointers instead of managing wrappers in C++

--- api/src/CLPModelWrapper_JNI.cpp
+++ api/src/CLPModelWrapper_JNI.cpp
@@ -1,9 +1,5 @@
 #include "CLPModel.hpp"
 #include <jni.h>
-#include <stdio.h>
-#include <unordered_map>
-#include <vector>
-#include <mutex>
 
 struct DoubleArrayMapping_t {
   jdoubleArray jarr;
@@ -39,50 +35,36 @@ public:
   }
 };
 
-// to handle multiple instances (from threads), store
-// instance-specific information in hash map (instead of passing pointers)
-std::unordered_map<jint, CLPModelWrapper *> wrappers;
-jint wrappers_position = 0;
-std::mutex mtx;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-JNIEXPORT jint JNICALL
+JNIEXPORT jlong JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1ctor(
     JNIEnv *, jobject, jint ncols, jint obj_sense) {
-  std::lock_guard<std::mutex> lg{mtx};
-  wrappers_position++;
-  wrappers.insert({wrappers_position, new CLPModelWrapper(ncols, obj_sense)});
-  // std::cout << "added wrapper " << wrappers_position
-  //           << " -> " << wrappers[wrappers_position] << std::endl;
-  return wrappers_position;
+  return (long) new CLPModelWrapper(ncols, obj_sense);
 }
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1dispose(
-    JNIEnv *, jobject, jint wrappers_i) {
+  JNIEnv *, jobject, jlong wrapper_ptr) {
   // for (auto clear : wrappers[wrappers_i]->clearup_double)
   //   env->ReleaseDoubleArrayElements(clear.jarr, clear.arr, JNI_ABORT);
-  mtx.lock();
-  delete wrappers[wrappers_i];
-  wrappers.erase(wrappers_i);
+  delete (CLPModelWrapper *) wrapper_ptr;
   // std::cout << "deleted wrapper " << wrappers_i << std::endl;
-  mtx.unlock();
 }
 
 JNIEXPORT jdouble JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1getInfinity(
-    JNIEnv *, jobject, jint wrappers_i) {
-  return wrappers[wrappers_i]->model->getInfinity();
+    JNIEnv *, jobject, jlong wrapper_ptr) {
+  return ((CLPModelWrapper *) wrapper_ptr)->model->getInfinity();
 }
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1setObjective(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_objective) {
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_objective) {
   auto len{env->GetArrayLength(j_objective)};
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   double *objective = env->GetDoubleArrayElements(j_objective, nullptr);
   wrapper->clearup_double.push_back({j_objective, objective});
   wrapper->model->setObjective(objective, len);
@@ -90,19 +72,19 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1setTimeLimit(
-    JNIEnv *, jobject, jint wrappers_i, jdouble seconds) {
+    JNIEnv *, jobject, jlong wrapper_ptr, jdouble seconds) {
   // std::cout << "*not* setting time limit " << seconds << std::endl;
   // std::cout << "setting time limit for wrapper " << wrappers_i;
-  // std::cout << " at address " << wrappers[wrappers_i];
-  // std::cout << " with model " << wrappers[wrappers_i]->model << std::endl;
-  wrappers[wrappers_i]->model->setTimeLimit(seconds);
+  // std::cout << " at address " << ((CLPModelWrapper *) wrapper_ptr);
+  // std::cout << " with model " << ((CLPModelWrapper *) wrapper_ptr)->model << std::endl;
+  ((CLPModelWrapper *) wrapper_ptr)->model->setTimeLimit(seconds);
 }
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1setColBounds(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_col_lb,
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_col_lb,
     jdoubleArray j_col_ub) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   double *col_lb = env->GetDoubleArrayElements(j_col_lb, nullptr);
   double *col_ub = env->GetDoubleArrayElements(j_col_ub, nullptr);
   auto len{env->GetArrayLength(j_col_lb)};
@@ -113,8 +95,8 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1setColStart(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_start) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_start) {
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   double *start = env->GetDoubleArrayElements(j_start, nullptr);
   auto len{env->GetArrayLength(j_start)};
   wrapper->clearup_double.push_back({j_start, start});
@@ -123,9 +105,9 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1addFullRow(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_row, jdouble lb,
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_row, jdouble lb,
     jdouble ub) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   double *row{env->GetDoubleArrayElements(j_row, nullptr)};
   auto len{env->GetArrayLength(j_row)};
   wrapper->model->addFullRow(row, len, lb, ub);
@@ -134,9 +116,9 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1addSparseRow(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_elems,
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_elems,
     jintArray j_indices, jdouble lb, jdouble ub) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   double *elems{env->GetDoubleArrayElements(j_elems, nullptr)};
   int *indices{env->GetIntArrayElements(j_indices, nullptr)};
   auto len{env->GetArrayLength(j_elems)};
@@ -147,17 +129,22 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1addSparseRowCached(
-    JNIEnv *env, jobject, jint wrappers_i, jdoubleArray j_elems,
+    JNIEnv *env, jobject, jlong wrapper_ptr, jdoubleArray j_elems,
     jintArray j_indices, jdouble lb, jdouble ub) {
   // std::cout << "adding sparse row (cached); wrapper_index " << wrappers_i
   //           << ", lb " << lb << ", ub " << ub << "; wrapper: ";
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   // std::cout << "ok, elems: ";
   double *elems{env->GetDoubleArrayElements(j_elems, nullptr)};
   // std::cout << "ok, indices: ";
   int *indices{env->GetIntArrayElements(j_indices, nullptr)};
   // std::cout << "ok, call successful: ";
+  // std::cout << "wrapper " << wrappers_i << " add row, first index "
+  // 	    << indices[0] << "\n";
   auto len{env->GetArrayLength(j_elems)};
+  // the `reserve` calls are supposed to save time, as we now
+  // beforehand how many elements we want to add. However, for some
+  // reason, the `size()` can get negative???
   wrapper->cached_elems.reserve(wrapper->cached_elems.size() + len);
   wrapper->cached_indices.reserve(wrapper->cached_indices.size() + len);
   for (int i{0}; i < len; ++i) {
@@ -174,10 +161,10 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT void JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1addSparseRows(
-    JNIEnv *env, jobject, jint wrappers_i, jint numrows, jintArray j_rowstarts,
+    JNIEnv *env, jobject, jlong wrapper_ptr, jint numrows, jintArray j_rowstarts,
     jdoubleArray j_elems, jintArray j_indices, jdoubleArray j_lb,
     jdoubleArray j_ub) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   int *rowstarts{env->GetIntArrayElements(j_rowstarts, nullptr)};
   double *elems{env->GetDoubleArrayElements(j_elems, nullptr)};
   int *indices{env->GetIntArrayElements(j_indices, nullptr)};
@@ -195,24 +182,32 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT jint JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1solve(
-    JNIEnv *, jobject, jint wrappers_i) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+    JNIEnv *, jobject, jlong wrapper_ptr) {
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   if (wrapper->cached_elems.size() > 0) {
     // assume everything is cached
     auto numrows{wrapper->cached_lb.size()};
     auto len{wrapper->cached_elems.size()};
+    auto maxindex{*std::max_element(wrapper->cached_indices.begin(), wrapper->cached_indices.end())};
+    // std::cout << "wrapper " << wrappers_i
+    // 	      << " indices " << &wrapper->cached_indices << " max index: "
+    // 	      << std::hex << maxindex
+    // 	      << ", len: " << len << ", numrows: " << numrows << "\n";
+    // if (maxindex > numrows)
+    //   std::cout << "!!!! ERRROR: largest index is higher than numrows !!!" << "\n";
     wrapper->model->addSparseRows(
         numrows, &wrapper->cached_rowstarts[0], &wrapper->cached_elems[0],
         &wrapper->cached_indices[0], len, &wrapper->cached_lb[0],
         &wrapper->cached_ub[0]);
   }
+  // std::cout << "solving wrapper " << wrappers_i << "\n";
   return static_cast<int>(wrapper->model->solve());
 }
 
 JNIEXPORT jdoubleArray JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1getColSolution(
-    JNIEnv *env, jobject, jint wrappers_i) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+    JNIEnv *env, jobject, jlong wrapper_ptr) {
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   auto len{wrapper->model->getNCols()};
   jdoubleArray colSolution{env->NewDoubleArray(len)};
   env->SetDoubleArrayRegion(colSolution, 0, len,
@@ -222,8 +217,8 @@ Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPMod
 
 JNIEXPORT jdouble JNICALL
 Java_de_unijena_bioinf_FragmentationTreeConstruction_computation_tree_ilp_CLPModel_1JNI_n_1getScore(
-    JNIEnv *, jobject, jint wrappers_i) {
-  CLPModelWrapper *wrapper{wrappers[wrappers_i]};
+    JNIEnv *, jobject, jlong wrapper_ptr) {
+  CLPModelWrapper *wrapper{((CLPModelWrapper *) wrapper_ptr)};
   return wrapper->model->getScore();
 }
 
